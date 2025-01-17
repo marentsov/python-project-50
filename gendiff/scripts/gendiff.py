582ab8ab6cd10a4504a8d2ea4f7084166e3075ef
marentsov <mr.marentsov@gmail.com>
2025-01-17 12:40:00 +0300

    second step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,9 @@ def parser_function():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument("-f", "--format", help='set format of output')
+
+
     args = parser.parse_args()
     print(args)
 
