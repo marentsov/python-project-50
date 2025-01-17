2433ceae09813b931b710373a155459c77478b7c
marentsov <mr.marentsov@gmail.com>
2025-01-17 09:03:35 +0300

    first step of the project

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,20 @@
 import argparse
 
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-args = parser.parse_args()
-print(args.first_file)
+def parser_function():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args)
+
+
+
+def main():
+    parser_function()
+
+
+if __name__ == "__main__":
+    main()
