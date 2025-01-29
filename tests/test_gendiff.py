38fe0c39f9ff68a44cd61a96679407caa1ba3707
marentsov <mr.marentsov@gmail.com>
2025-01-29 19:07:26 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,20 +1,25 @@
 import pytest
 
 from gendiff.scripts.generate_diff import generate_diff
-from gendiff.scripts.parser import parse_data_from_file, read_file
+from gendiff.scripts.parser import  parse_data_from_file, read_file
 
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
     ('tests/test_data/file1.json',
      'tests/test_data/file2.json',
-     'tests/test_data/expected_result_json.txt'),
+     'tests/test_data/expected_result_plain.txt'),
     ('tests/test_data/file1.yaml',
      'tests/test_data/file2.yaml',
      'tests/test_data/expected_result_yaml.txt')])
 def test_generate_diff(file_path1, file_path2, expected_result):
-    diff = generate_diff(file_path1, file_path2, formatter="stylish")
+    diff = generate_diff(file_path1, file_path2, formatter="plain")
     expected = read_file(expected_result).strip()
     assert diff.strip() == expected
+
+
+
+
+
 
 
 
