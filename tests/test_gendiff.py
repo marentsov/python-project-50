c58eb5a0911c4a01d500b859dbb929af84b91809
marentsov <mr.marentsov@gmail.com>
2025-01-29 19:29:33 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,20 @@
 import pytest
 
 from gendiff.scripts.generate_diff import generate_diff
-from gendiff.scripts.parser import  parse_data_from_file, read_file
+from gendiff.scripts.parser import read_file
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_json.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_yaml.txt')])
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2)
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
 
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
@@ -10,8 +23,8 @@ from gendiff.scripts.parser import  parse_data_from_file, read_file
      'tests/test_data/expected_result_plain.txt'),
     ('tests/test_data/file1.yaml',
      'tests/test_data/file2.yaml',
-     'tests/test_data/expected_result_yaml.txt')])
-def test_generate_diff(file_path1, file_path2, expected_result):
+     'tests/test_data/expected_result_plain.txt')])
+def test_generate_diff_plain(file_path1, file_path2, expected_result):
     diff = generate_diff(file_path1, file_path2, formatter="plain")
     expected = read_file(expected_result).strip()
     assert diff.strip() == expected
