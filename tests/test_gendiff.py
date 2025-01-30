3cd542c49c86812a03698cd7cf597cc5eebd732f
marentsov <mr.marentsov@gmail.com>
2025-01-30 14:26:24 +0300

    fix tests with new format json

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -30,6 +30,17 @@ def test_generate_diff_plain(file_path1, file_path2, expected_result):
     assert diff.strip() == expected
 
 
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_json_format.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_json_format.txt')])
+def test_generate_diff_json(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="json")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
 
 
 
