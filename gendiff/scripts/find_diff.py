18d1934e815f37b35db4e4a572ceb971f4dc5bfe
marentsov <mr.marentsov@gmail.com>
2025-01-30 13:32:29 +0300

    fix gendiff logic for empty values, fix tests

--- gendiff/scripts/find_diff.py
+++ gendiff/scripts/find_diff.py
@@ -31,6 +31,15 @@ def items_modified(key, value1, value2):
     }
 
 
+def items_modified_from_empty(key, value1, value2):
+    return {
+        'action': 'empty',
+        'name': key,
+        'new_value': value2,
+        'old_value': value1
+    }
+
+
 def items_nested(key, value1, value2):
     return {
         'action': 'nested',
@@ -56,7 +65,9 @@ def find_diff(data1, data2):
             diff.append(item_delete(key, value1))
         elif isinstance(value1, dict) and isinstance(value2, dict):
             diff.append(items_nested(key, value1, value2))
-        elif value1 != value2:
+        elif value1 != value2 and value1 == "":
+            diff.append(items_modified_from_empty(key, value1, value2))
+        elif value1 != value2 and value1 != "":
             diff.append(items_modified(key, value1, value2))
         else:
             diff.append(items_unchanged(key, value1))
